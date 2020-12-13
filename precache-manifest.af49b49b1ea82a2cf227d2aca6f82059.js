2bcf1ac46b1f6ef11d5421acc4fb2153ecd3804f
Fcater <fc1303140304@gmail.com>
2020-12-13 22:39:55 +0800

    修改homepage为:./

---
+++ precache-manifest.af49b49b1ea82a2cf227d2aca6f82059.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1306a3401ae07ea857f110707ab08b46",
+    "url": "./index.html"
+  },
+  {
+    "revision": "414333fbe37788afe60a",
+    "url": "./static/css/main.60147484.chunk.css"
+  },
+  {
+    "revision": "77d0173fddf7cc17a458",
+    "url": "./static/js/2.5c921bf5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.5c921bf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "414333fbe37788afe60a",
+    "url": "./static/js/main.d9635044.chunk.js"
+  },
+  {
+    "revision": "61c51d826557577329a5",
+    "url": "./static/js/runtime-main.81b8e286.js"
+  }
+]);
