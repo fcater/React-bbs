a7a349631e3d97202c58f3e7fafaaf2a5e195c98
Fcater <fc1303140304@gmail.com>
2020-12-14 21:37:19 +0800

    重新发布了适配的网页

---
+++ precache-manifest.58f7a71fbe35385a6527c8841e8861f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8249a99e720f705fee8296573aefbdaa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eaa3b06bf0561ba45e89",
+    "url": "./static/css/main.4f1bd764.chunk.css"
+  },
+  {
+    "revision": "be137b13bf775e2d2a2d",
+    "url": "./static/js/2.c8b21fcb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.c8b21fcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaa3b06bf0561ba45e89",
+    "url": "./static/js/main.601bb879.chunk.js"
+  },
+  {
+    "revision": "61c51d826557577329a5",
+    "url": "./static/js/runtime-main.81b8e286.js"
+  }
+]);
